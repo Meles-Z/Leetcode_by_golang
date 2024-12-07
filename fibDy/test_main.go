0f39789fa3707ca71e8a44ba904c473d544ec4bc
Meles-Z <meles.zawdie@gmail.com>
2024-12-07 09:46:28 +0300

    fibonnaci with dynamic algorithm

---
+++ fibDy/test_main.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func Benchmark_fibonnaci(b testing.B){
+	b.ReportAllocs()
+	for i:=0; i<b.N; i++{
+		fibonacci(50)
+	}
+}
+func Benchmark_memoization(b *testing.B) {
+	b.ReportAllocs()
+
+	for i:=0; i<b.N;i++{
+		memoization(25)
+	}
+}
+
+func Benchmark_tabulation(b *testing.B) {
+	b.ReportAllocs()
+
+	for i:=0; i<b.N;i++{
+		tabulation(25)
+	}
+}
+
+func Benchmark_fibDynamic(b *testing.B) {
+	b.ReportAllocs()
+
+	for i:=0; i<b.N;i++{
+		fibDynamic(25)
+	}
+}
