45e2ac4f115af554473d0eb470c4b08a5271dc82
Meles-Z <meles.zawdie@gmail.com>
2024-12-12 12:00:17 +0300

    docker file

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
-func singleNumber(nums []int) int {
-	result := 0
-	  for _, val:= range nums{
-		  result^=val
-	  }
-	  return result
-  }
+// import "fmt"
 
-func main() {
+// func hashMap(arr1, arr2 []int) []int {
+// 	map1:=make(map[int]int)
+// 	map2:=make(map[int]int)
+// 	for i:=0; i<len(arr1); i++{
+// 	}
+// 	return map1
+// }
 
-}
+// func main() {
+// 	arr1 := []int{1, 2, 4, 3}
+// 	arr2 := []int{3, 5, 1, 6}
+// 	fmt.Println(hashMap(arr1, arr2))
+// }
