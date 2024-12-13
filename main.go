9eb7790d7d5f05acd3c3fdee3d4ef0e591e56a8b
Meles-Z <meles.zawdie@gmail.com>
2024-12-13 13:45:34 +0300

    string anagram

--- main.go
+++ main.go
@@ -2,52 +2,21 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-func frequencyOfCharacter(s string) map[string]int {
-	memo := make(map[string]int)
-	for _, char := range s {
-		memo[string(char)]++
-	}
-	return memo
-}
-
-func findFirstCharacterNonReate(s string) rune {
-	memo := make(map[rune]int)
-	// calculate all the frequencey
-	for _, char := range s {
-		memo[char]++
-	}
-	// filter the frequnecy only one
-	for _, char := range s {
-		if memo[char] == 1 {
-			return char
+func missedArray(nums []int) []int {
+	sort.Ints(nums)
+	missed := []int{}
+	for i := 0; i < len(nums)-1; i++ {
+		for j := nums[i] + 1; j < nums[i+1]; j++ {
+			missed = append(missed, j)
 		}
 	}
-	return '_'
+	return missed
 }
 
-func anagrams(s string, p string) bool {
-	if len(s) != len(p) {
-		return false
-	}
-	mapS := make(map[rune]int)
-	mapT := make(map[rune]int)
-	for _, char := range s {
-		mapS[char]++
-	}
-	for _, char := range p {
-		mapT[char]++
-	}
-
-	for i, value := range mapS {
-		if mapT[i] != value {
-			return false
-		}
-	}
-	return true
-
-}
 func main() {
-	fmt.Println(anagrams("meles", "seler"))
+	fmt.Println(missedArray([]int{9, 6, 2, 3, 5, 7, 0}))
+
 }
