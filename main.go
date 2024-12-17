46ac67253955438fe08f70213b01fd568637d5bd
Meles-Z <meles.zawdie@gmail.com>
2024-12-17 10:34:20 +0300

    three sum value

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
-func main(){
-    
+import "fmt"
+
+func contains()
+
+
+func main() {
+	
 }
---
+++ removeElemet/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func removeElement(nums []int, val int) int {
+    k:=0
+    for i:=0; i<len(nums); i++{
+        if nums[i] !=val{
+           nums[k]=nums[i]
+        }
+    }
+    return k
+}
+func main(){
+	fmt.Println(removeElement([]int{3,2,2,3}, 3))
+	fmt.Println(removeElement([]int{0,1,2,2,3,0,4,2}, 2))
+}
