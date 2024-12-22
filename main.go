ad652083d7f154cc14d8e4600fe76aae101c21bf
Meles-Z <meles.zawdie@gmail.com>
2024-12-22 23:21:33 +0300

    matrix add and identity is added

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main() {
-	for i := 1; i <= 5; i++ {
-		str := ""
-		for j := 1; j <= i; j++ {
-			str += "*"
-		}
-		fmt.Println(str)
+func Identity(order int) [][]float64 {
+	matrix := make([][]float64, order)
+	for i := 0; i < order; i++ {
+		row := make([]float64, order)
+		matrix[i] = row
+		row[i] = 1
 	}
+	return matrix
+}
+
+func AddMatrix(mat1 [2][2]int, mat2 [2][2]int) [2][2]int {
+    var sum [2][2]int
+    for l:=0; l<2; l++{
+        for j:=0; j<2; j++{
+            sum[l][j]=mat1[l][j]+mat2[l][j]
+        }
+    }
+    return sum
+}
+func main() {
+	fmt.Println(Identity(4))
+    fmt.Println(AddMatrix([2][2]int{{1,2},{1,2}}, [2][2]int{{3,4},{5,6}}))
+
 }
