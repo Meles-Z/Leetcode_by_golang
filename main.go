163cce4f03b5b01016b57553dc2455e7ce0b2bbe
Meles-Z <meles.zawdie@gmail.com>
2024-12-25 10:20:48 +0300

    context in golang

--- main.go
+++ main.go
@@ -1,17 +1,5 @@
 package main
 
-import "fmt"
+func main(){
 
-func construct2DArray(original []int, m int, n int) [][]int {
-	if len(original) != m*n {
-		return [][]int{}
-	}
-	result := make([][]int, m)
-	for i := 0; i < m; i++ {
-		result[i] = original[i*n : (i+1)*n]
-	}
-	return result
-}
-func main() {
-	fmt.Println(construct2DArray([]int{1, 2, 3, 4}, 2, 2))
 }
