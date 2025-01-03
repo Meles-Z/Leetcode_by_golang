9fc0b73dfcde4910c19450344d2e148f995d21d9
Meles-Z <meles.zawdie@gmail.com>
2025-01-03 10:13:30 +0300

    all golang file is added

--- main.go
+++ main.go
@@ -1,45 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
-func compare(a, b []int) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i := range a {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
-}
-func exist(nums [][]int, target []int) bool {
-	for _, val := range nums {
-		if compare(val, target) {
-			return true
-		}
-	}
-	return false
-}
-func permute(nums []int) [][]int {
-	if len(nums) == 0 {
-		return [][]int{{}}
-	}
-	var result [][]int
-	for i, num := range nums {
-		res := append([]int{}, nums[:i]...)
-		res = append(res, nums[i+1:]...)
-		for _, perm := range permute(res) {
-			newPerm := append([]int{num}, perm...)
-			if !exist(result, newPerm) {
-				result = append(result, newPerm)
-			}
-		}
-	}
 
-	return result
+func Authentication() echo.MiddlewareFunc{
+	cnfg:=middleware.JWTConfig{}
 }
+func main(){
 
-func main() {
-	fmt.Println(permute([]int{1, 1, 2}))
 }
