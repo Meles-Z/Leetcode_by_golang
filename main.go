be2fcf7131ac5385ee209189dc331e6b5b09c0ec
Meles-Z <meles.zawdie@gmail.com>
2025-01-15 16:49:33 +0300

    2D array

--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import "fmt"
 
+func returnMatrix(rows, col int) [][]int {
+	matrix := make([][]int, rows)
+	for i := range matrix {
+		matrix[i] = make([]int, col)
+	}
+	return matrix
+}
+func main() {
 
-func main(){
-	const n=5
-	var name=n
-	fmt.Println(name)
-	fmt.Println("Somechanges...\n")
+	matrix := returnMatrix(3, 4)
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 4; j++ {
+			matrix[i][j] = i*3 + j + 1
+		}
+	}
+	for _, row:=range matrix{
+		fmt.Println( row)
+	}
 }
