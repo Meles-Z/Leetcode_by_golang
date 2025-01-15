3bb5d1424c96771f507f413eec63ae521de325a4
Meles-Z <meles.zawdie@gmail.com>
2025-01-15 17:26:53 +0300

    2D array

--- main.go
+++ main.go
@@ -2,22 +2,38 @@ package main
 
 import "fmt"
 
-func returnMatrix(rows, col int) [][]int {
+func returnTwoDimenstion(rows, cols int) [][]int {
 	matrix := make([][]int, rows)
 	for i := range matrix {
-		matrix[i] = make([]int, col)
+		matrix[i] = make([]int, cols)
+	}
+
+	return matrix
+}
+
+func threeDimensions(rows, cols int) [][][]int {
+	matrix := make([][][]int, rows)
+	for i:=range matrix{
+		matrix[i]=make([][]int, rows)
+		for j:=range matrix[i]{
+			matrix[i][j]=make([]int, cols)
+		}
 	}
 	return matrix
 }
 func main() {
-
-	matrix := returnMatrix(3, 4)
-	for i := 0; i < 3; i++ {
-		for j := 0; j < 4; j++ {
-			matrix[i][j] = i*3 + j + 1
+	row := 3
+	cols := 4
+	matrix := returnTwoDimenstion(row, cols)
+	for i := 0; i < row; i++ {
+		for j := 0; j < cols; j++ {
+			matrix[i][j] = i*cols + j + 1
 		}
 	}
-	for _, row:=range matrix{
-		fmt.Println( row)
+	for i := 0; i < len(matrix); i++ {
+
+		fmt.Println(matrix[i])
 	}
+  matr:=threeDimensions(row, cols)
+  fmt.Println("three Dimesnsiton", matr)
 }
