b107e0faa9c6204987e5c53bd447a1091969f36c
Meles-Z <meles.zawdie@gmail.com>
2025-01-15 11:25:49 +0300

    args in fmt package

--- main.go
+++ main.go
@@ -1,33 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"strconv"
-)
+import "fmt"
 
-func sum(num1, num2 int) int {
-	return num1 + num2
-}
 
-func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Your input need like this format: go run main.go <num1> <num2>")
-		return
-	}
-    
-	arg1 := os.Args[1]
-	arg2 := os.Args[2]
-
-	num1, err := strconv.Atoi(arg1)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	num2, err := strconv.Atoi(arg2)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("The sum of %[1]d + %[2]d = %d\n", num1, num2, sum(num1, num2))
+func main(){
+	const n=5
+	var name=n
+	fmt.Println(name)
+	fmt.Println("Somechanges...\n")
 }
