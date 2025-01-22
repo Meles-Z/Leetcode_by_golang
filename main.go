78acc53b6c1ee503367384af737088fe3a7ac70b
Meles-Z <meles.zawdie@gmail.com>
2025-01-22 22:55:37 +0300

    some change

--- main.go
+++ main.go
@@ -1,47 +1,11 @@
 package main
 
 import (
-	"context"
+	"bytes"
 	"fmt"
-	"io"
-	"net/http"
-	"time"
 )
 
-func webScrapper(ctx context.Context, url string, result chan<- string) {
-	client := http.DefaultClient
-	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
-	if err != nil {
-		result <- fmt.Sprintf("Error to create reques:%s", err)
-		return
-	}
-	resp, err := client.Do(req)
-	if err != nil {
-		result <- fmt.Sprintf("Error to send request:%s", err)
-		return
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		result <- fmt.Sprintf("Error to read body file:%s", err)
-		return
-	}
-	result <- fmt.Sprintf("The result of request body:%s", string(body))
-}
-
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	urls := []string{
-		"https://api.example.com/users",
-		"https://api.example.com/products",
-		"https://api.example.com/orders",
-	}
-	ch := make(chan string)
-	for _, url := range urls {
-		go webScrapper(ctx, url, ch)
-	}
-	for range urls {
-		fmt.Println(<-ch)
-	}
+	data := []byte("example")
+	fmt.Println(bytes.Count(data, []byte("e"))) // Output: 2
 }
