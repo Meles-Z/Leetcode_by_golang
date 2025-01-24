f2ebd573796748a2f23a253016a4f44c3d68dc4f
Meles-Z <meles.zawdie@gmail.com>
2025-01-24 17:17:17 +0300

    mastering on go rutins

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
+	"math/rand"
+	"strings"
+	"time"
 )
 
+func say(id int, phrase string) {
+	for _, word := range strings.Fields(phrase) {
+		fmt.Printf("Worked %d says: %s...\n", id, word)
+		dur := time.Duration(rand.Intn(100)) * time.Microsecond
+		time.Sleep(dur)
+	}
+}
+
 func main() {
-	data := []byte("example")
-	fmt.Println(bytes.Count(data, []byte("e"))) // Output: 2
+	say(1, "Golang is the best progg!")
+	say(2, "What is going on")
 }
