795eea5f7b4c9ba6baec7a2f3796945ce76e1999
Meles-Z <meles.zawdie@gmail.com>
2025-02-03 16:58:20 +0300

    testing kubernates

--- main.go
+++ main.go
@@ -2,49 +2,8 @@ package main
 
 import "fmt"
 
-type TestStruct struct {
-	Name    string
-	Email   string
-	Address struct {
-		City   string
-		Post   string
-		Street string
-	}
-}
-
-type OptsTest struct {
-	test []TestStruct
-}
-
-func (t *OptsTest) addTest(name, email string) {
-	testStr := TestStruct{
-		Name:  name,
-		Email: email,
-		Address: struct{City string; Post string; Street string}{
-			City: "AA",
-			Post: "123",
-			Street: "Shshs",
-		},
-	}
-	t.test = append(t.test, testStr)
-}
+// This is a comment
 
 func main() {
-	test := OptsTest{}
-	test.addTest("F", "f@gmail.com")
-	test.addTest("T", "t@gmail.com")
-	test.addTest("F", "f@gmail.com")
-	test.addTest("T", "t@gmail.com")
-	for _, v := range test.test {
-		fmt.Println(v)
-	}
-
-	app:=struct{
-		version string
-		lastUpdate string
-	}{
-		version: "v1.1",
-		lastUpdate: "jan 12, 2012",
-	}
-	fmt.Println(app)
+	fmt.Println("Hello, world!")
 }
