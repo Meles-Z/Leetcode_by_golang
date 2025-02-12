2bfef532aa0432ebd1dccf93d206322feefc5569
Meles-Z <meles.zawdie@gmail.com>
2025-02-12 14:29:29 +0300

    learning wg and gorutines

--- main.go
+++ main.go
@@ -1,10 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-// This is a comment
-
-
+func runAllFunc(wg *sync.WaitGroup, fun ...func()) {
+	wg.Add(len(fun))
+	for _, fn := range fun {
+		go func ()  {
+			defer wg.Done()
+			 fn()
+			
+		}()
+	}
+}
 func main() {
-	fmt.Println("Hello, world!")
+	var wg sync.WaitGroup
+
+	test1 := func() {
+		fmt.Println("Hi there one")
+	}
+
+	test2 := func() {
+		fmt.Println("Hi there 2")
+	}
+
+	func3 := func() {
+		fmt.Println("Hi there3")
+	}
+
+	runAllFunc(&wg, test1, test2, func3)
+	wg.Wait()
+
+	now := time.Now()
+	elipse := time.Since(now)
+	fmt.Println(elipse)
 }
